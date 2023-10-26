f54f5a5128c5bf40bd874cecc2736080e7dbd336
Audrey Mengue <massoumeharmonie@gmail.com>
2023-10-26 02:22:15 +0100

    Added mamin banner

--- src/pages/Mbolosamba.jsx
+++ src/pages/Mbolosamba.jsx
@@ -1,22 +1,32 @@
 import { Box, Divider, Grid, GridItem, Flex, Heading, Text } from '@chakra-ui/react'
 import muggalogo from '../images/muggalogo.png'
+import newbg from '../images/bg.jpg'
+import bg from '../images/images.jpg'
 import React from 'react'
 
 const Mbolosamba = () => {
     return (
-        <Flex bg="gray.50" justifyContent="space-between">
-            <Box bg="red.200" w="50%" textAlign="center" >
-                <Box mx="auto" my="200px">
-                    <Heading as="h1">
-                        Microsoft User Group Gabon
-                    </Heading>
-                    <Text> Pour les passionés des technologies Microsoft </Text>
-                </Box>
+        <Flex
+            backgroundImage={newbg}
+            backgroundPosition="center"
+            backgroundRepeat="no-repeat"
+            justifyContent="space-between"
+            bgSize="cover"
+            textAlign="center"
+            mx="50px" mt="10px"
+            borderRadius="10px"
+        >
+            <Box mx="auto" my="100px" py="20px" px="10px" borderRadius="10px" border="2px" borderColor="yellow.300">
+                <Heading as="h1" color="white">
+                    Microsoft User Group Gabon
+                </Heading>
+                <Text color="white"> Pour les passionnés des technologies Microsoft </Text>
             </Box>
+
             {/* <Divider /> */}
-            <Box textAlign="center">
-                <img src={muggalogo} style={{ width: '500px' }} alt='The logo of Microsft User Group Gabon' />
-            </Box>
+            {/* <Box textAlign="center">
+                <img src={gabon9} style={{ width: '400px' }} alt='The logo of Microsft User Group Gabon' />
+            </Box> */}
         </Flex>
     )
 }
