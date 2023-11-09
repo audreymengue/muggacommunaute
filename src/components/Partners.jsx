13aac719c6d509ee5e8302f698b94cb0972966f7
Audrey Mengue <massoumeharmonie@gmail.com>
2023-11-09 10:15:41 +0100

    Added footer

--- src/components/Partners.jsx
+++ src/components/Partners.jsx
@@ -12,8 +12,8 @@ export default function Partners() {
         slidesToShow: 1,
         slidesToScroll: 1,
         autoplay: true,
-        speed: 2000,
-        autoplaySpeed: 2000,
+        speed: 5000,
+        autoplaySpeed: 5000,
         cssEase: "linear"
     };
     return (
--- src/layouts/RootLayout.jsx
+++ src/layouts/RootLayout.jsx
@@ -3,6 +3,7 @@ import NavBar from '../components/NavBar'
 import { Box, Divider, Flex } from '@chakra-ui/react'
 import Mbolosamba from '../pages/Mbolosamba'
 import Partners from '../components/Partners'
+import Footer from '../components/Footer'
 
 const RootLayout = () => {
     return (
@@ -11,6 +12,7 @@ const RootLayout = () => {
             <Divider orientation='horizontal' />
             <Mbolosamba />
             <Partners />
+            <Footer />
         </Flex>
     )
 }
