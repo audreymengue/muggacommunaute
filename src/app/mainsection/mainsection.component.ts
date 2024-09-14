cd7ed2762386da0bcf8fbe1cefa08b430b52118a
lamengs <mac@macs-MacBook-Pro-2.local>
2024-09-14 11:38:07 +0100

    Added footer

---
+++ src/app/mainsection/mainsection.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-mainsection',
+  standalone: true,
+  imports: [],
+  templateUrl: './mainsection.component.html',
+  styleUrl: './mainsection.component.css'
+})
+export class MainsectionComponent {
+
+}
